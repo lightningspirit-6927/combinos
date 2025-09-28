fd91950dec5eed58aed8f4a8c61d7289c14ffbd4
lightningspirit-6927 <giahuy251110@gmail.com>
2025-09-28 17:24:41 +0700

    what

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const DEFAULT_HANDLING = {
-    das: 60,
+    das: 100,
     arr: 0,
-    sdr: 4 // soft drop rate, in ms
+    sdr: 0 // soft drop rate, in ms
 };
 
 const DEFAULT_KEYS = {
@@ -9,9 +9,27 @@ const DEFAULT_KEYS = {
     moveRight: ["ArrowRight"],
     softDrop: ["ArrowDown"],
     hardDrop: ["Space"],
+    rotateCW: ["KeyX", "ArrowUp"],
+    rotateCCW: ["ControlLeft", "ControlRight", "KeyZ"],
+    rotate180: ["KeyA"],
+    hold: ["ShiftLeft", "ShiftRight", "KeyC"],
+    retry: ["KeyR"],
+}
+const myKeys = {
+    moveLeft: ["ArrowLeft"],
+    moveRight: ["ArrowRight"],
     rotateCW: ["ArrowUp"],
     rotateCCW: ["KeyZ"],
-    rotate180: ["KeyA"],
+    rotate180: ["KeyX"],
+    softDrop: ["ArrowDown"],
+    hardDrop: ["Space"],
     hold: ["KeyC"],
     retry: ["KeyQ"],
+};
+const myHandling = {
+    das: 60,
+    arr: 0,
+    sdr: 4 // soft drop rate in ms
 }
+localStorage.setItem("KEYS", JSON.stringify(myKeys));
+localStorage.setItem("HANDLING", JSON.stringify(myHandling));
